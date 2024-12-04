2b4e2619c535f977610299137d3c60cf04f5a444
zsudedib <114497057+zsudedib@users.noreply.github.com>
2024-12-03 22:21:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const sendMail = () => {
     let params = {
-        formName: $("#form-name")[0].value,
-        formEmail: $("#form-email")[0].value,
-        formMessage: $("#form-message")[0].value,
+        formName: $(".form-name")[0].value,
+        formEmail: $(".form-email")[0].value,
+        formMessage: $(".form-message")[0].value,
     }
 
     emailjs.send("service_jotpb66","template_kpo2rhr",params).then(alert('Message sent!'))
