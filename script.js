b6e8be7d74b8fa2238e0b71d2a83c974718dce59
zsudedib <114497057+zsudedib@users.noreply.github.com>
2024-12-04 09:32:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 console.log("Not sure why you're in the console, but regardless, thanks for checking out my personal website!");
 const sendMail = () => {
     let params = {
-        formName: $(".form-name")[0].value,
+        formFirstName: ($(".form-name")[0].value.trim() + " ").split(" ")[0],
+        formLastName: ($(".form-name")[0].value.trim() + " ").split(" ").splice(1).join(" ").trim(),
         formEmail: $(".form-email")[0].value,
         formMessage: $(".form-message")[0].value,
     }
