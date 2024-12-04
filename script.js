84a72fb8e2fab5176281fccf3fede0e29403d7f8
zsudedib <114497057+zsudedib@users.noreply.github.com>
2024-12-03 23:32:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+console.log("Not sure why you're in the console, but regardless, thanks for checking out my personal website!");
 const sendMail = () => {
     let params = {
         formName: $(".form-name")[0].value,
         formEmail: $(".form-email")[0].value,
         formMessage: $(".form-message")[0].value,
     }
-
-    emailjs.send("service_jotpb66","template_kpo2rhr",params).then(alert('Message sent!'))
-    console.log("Sent!");
+    // console.log(params);
+    emailjs.send("service_jotpb66","template_kpo2rhr",params);
+    // console.log("Sent message");
 }
